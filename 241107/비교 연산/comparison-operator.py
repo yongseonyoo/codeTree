34372eca481a2a7d0efda8abfef71d2edacb2105
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-07 17:44:04 +0900

    Title: 비교 연산, Time: 46ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241107/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+n = input()
+arr = n.split()
+a, b = int(arr[0]), int(arr[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+if b > a:
+    print(1)
+else:
+    print(0)
+
+if a == b:
+    print(1)
+else:
+    print(0)
+
+if a != b:
+    print(1)
+else:
+    print(0)
