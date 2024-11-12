7ce204df6e5cbbf4130201665488aa98996893e9
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-12 19:03:09 +0900

    Title: 4가지 관계연산자, Time: 41ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241112/4가지 관계연산자/four-relational-operators.py
@@ -0,0 +1,23 @@
+n = input()
+arr = n.split()
+a, b = int(arr[0]), int(arr[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+if b > a:
+    print(1)
+else:
+    print(0)
