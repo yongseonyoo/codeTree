99d8b4ef6f5c67ef226fa1abba96088841cef6ed
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-12 19:05:52 +0900

    Title: 4번의 크기 비교, Time: 45ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241112/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,26 @@
+a = int(input())
+
+n = input()
+arr = n.split()
+b, c, d, e = int(arr[0]), int(arr[1]), int(arr[2]), int(arr[3])
+
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if a > c:
+    print(1)
+else:
+    print(0)
+
+if a > d:
+    print(1)
+else:
+    print(0)
+
+if a > e:
+    print(1)
+else:
+    print(0)
