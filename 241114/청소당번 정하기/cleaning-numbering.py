2e4231ff90df3a8a0f96ef57aa55a8e377350b73
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-14 14:54:37 +0900

    Title: 청소당번 정하기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241114/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+cls_cnt = 0
+hal_cnt = 0
+toi_cnt = 0
+
+for i in range(n):
+    if i != 0:
+        if i % 12 == 0:
+            toi_cnt += 1
+        elif i % 3 == 0:
+            hal_cnt += 1
+        elif i % 2 == 0:
+            cls_cnt += 1
+
+print(cls_cnt, hal_cnt, toi_cnt)
