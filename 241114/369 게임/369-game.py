1448633082dd16d3571cadac082ab2ba1103ec20
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-14 13:53:58 +0900

    Title: 369 게임, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/369 게임/369-game.py
+++ 241114/369 게임/369-game.py
@@ -1,7 +1,9 @@
 n = int(input())
 
 for i in range(1, n+1, 1):
-    if i % 3 == 0:
+    if '3' in str(i) or '6' in str(i) or '9' in str(i):
+        print(0, end=' ')
+    elif i % 3 == 0:
         print(0, end=' ')
     else:
         print(i, end=' ')
