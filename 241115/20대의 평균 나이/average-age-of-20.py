e81a94e35625ace1ada1d1d65db02e8380eeca5a
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-15 13:58:12 +0900

    Title: 20대의 평균 나이, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241115/20대의 평균 나이/average-age-of-20.py
+++ 241115/20대의 평균 나이/average-age-of-20.py
@@ -1,10 +1,13 @@
 prob = 0
 cnt = 0
 while True:
-    age = int(input())
-    if age >= 30:
-        print(f"{prob/cnt:.2f}")
+    try:
+        age = int(input())
+        if age >= 30:
+            print(f"{prob/cnt:.2f}")
+            break
+        else:
+            prob += age
+            cnt += 1
+    except:
         break
-    else:
-        prob += age
-        cnt += 1
