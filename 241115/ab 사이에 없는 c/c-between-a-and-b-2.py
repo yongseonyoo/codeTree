a894ea86b4f716b4643063e1e261581da9432dc5
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2024-11-15 16:24:32 +0900

    Title: ab 사이에 없는 c, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241115/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 241115/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -4,10 +4,10 @@ a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
 
 swi = False
 for i in range(a, b+1):
-    if c % i == 0:
+    if i % c == 0:
         swi = True
 
 if swi == True:
-    print('YES')
-else:
     print('NO')
+else:
+    print('YES')
