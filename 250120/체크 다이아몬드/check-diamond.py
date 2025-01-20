2988b0aad16b328ee3f86956da2fee82f908ca07
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-20 17:14:14 +0900

    Title: 체크 다이아몬드, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250120/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    # print(n-i-1, i+1)
+    print(" " * (n-i-1), end="")
+    print("* " * (i+1))
+
+for i in range(n-1):
+    # print(i+1, n-1-i)
+    print(" " * (i+1), end="")
+    print("* " * (n-1-i))
