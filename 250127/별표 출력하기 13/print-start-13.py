a72dac5b989c4c186cceeb51c877e6bb04670b26
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-27 21:26:16 +0900

    Title: 별표 출력하기 13, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/별표 출력하기 13/print-start-13.py
+++ 250127/별표 출력하기 13/print-start-13.py
@@ -1,21 +1,11 @@
 n = int(input())
 
-odd = 0
-even = 0
-for i in range(n):
+even, odd = 0, 0
+
+for i in range(2 * n):
     if i % 2 == 0:
         print("* " * (n - even))
         even += 1
     else:
         print("* " * (odd + 1))
-        odd += 1
-
-odd = 0
-even = 0
-for i in range(n):
-    if i % 2 == 0:
-        print("* " * (even + 3))
-        even += 1
-    else:
-        print("* " * (n - odd - 3))
         odd += 1
