dabe5598cff3698551b048491b227c1144332d19
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-28 14:24:24 +0900

    Title: 별표 출력하기 11, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250128/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(2 * n + 1):
+    if i % 2 == 0:
+        print("* " * (2 * n + 1))
+    else:
+        for j in range(2 * n + 1):
+            if j % 2 == 0:
+                print('* ', end='')
+            else:
+                print('  ', end='')
+        print()
