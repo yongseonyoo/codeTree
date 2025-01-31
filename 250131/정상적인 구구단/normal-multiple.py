73ea83ac328b8b590b338351258a0f496dd3d9c9
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-31 15:52:26 +0900

    Title: 정상적인 구구단, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250131/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        print(f"{i+1} * {j+1} = {(i+1) * (j+1)}", end='')
+        if j < (n-1):
+            print(',', end=' ')
+    print()
