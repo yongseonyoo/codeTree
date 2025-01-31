9f87398b688495f6d386cbf32a9d9b9dbe24e67b
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-31 16:02:07 +0900

    Title: 구구단 만들기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250131/구구단 만들기/print-multiplication-table.py
@@ -0,0 +1,15 @@
+n = input()
+arr = n.split()
+a, b = int(arr[0]), int(arr[1])
+
+cnt = b
+for i in range(1, 10):
+    for j in range(3):
+        if cnt <= b and cnt >= a:
+            print(f"{cnt} * {i} = {cnt * i}", end='')
+            if cnt != a:
+                print(' / ', end ='')
+            cnt -= 2
+    cnt = b
+    print()
+    
