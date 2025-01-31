4bdeaf8a7fba26dad5e69d2d46706ce898941f9c
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-01-31 15:18:31 +0900

    Title: 9-1 정사각형, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250131/9-1 정사각형/9-1-square.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+cnt = 9
+for i in range(n):
+    for j in range(n):
+        print(cnt, end='')
+        if cnt == 1:
+            cnt = 9
+        else:
+            cnt -= 1
+    print()
