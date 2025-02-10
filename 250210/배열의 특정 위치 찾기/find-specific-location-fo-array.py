b3e334046b883a5e20606e0f9de7a2c084606f3a
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-10 12:34:16 +0900

    Title: 배열의 특정 위치 찾기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250210/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+
+even_val = 0
+multi_val = 0
+multi_cnt = 0
+for i in range(1, len(arr)+1):
+    if i % 2 == 0:
+        even_val += arr[i-1]
+    if i % 3 == 0:
+        multi_val += arr[i-1]
+        multi_cnt += 1
+
+multi_mean = multi_val / multi_cnt
+print(even_val, multi_mean)
