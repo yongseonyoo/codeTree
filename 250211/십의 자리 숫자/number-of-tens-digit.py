c847ebf23ccfcfc4333b473e20df483d8cf2f3de
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-11 17:47:51 +0900

    Title: 십의 자리 숫자, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250211/십의 자리 숫자/number-of-tens-digit.py
+++ 250211/십의 자리 숫자/number-of-tens-digit.py
@@ -3,6 +3,8 @@ arr = list(map(int, input().split()))
 for i in range(1, 10):
     cnt = 0
     for j in arr:
-        if j // 10 == i and j >= 10:
+        if j == 0:
+            break
+        elif j // 10 == i and j >= 10:
             cnt += 1
     print(f"{i} - {cnt}") 
