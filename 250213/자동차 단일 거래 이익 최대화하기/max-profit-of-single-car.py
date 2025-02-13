84d3bddfc1cd36cbd1e4872a444e6d273d2f9beb
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-13 16:55:31 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 60ms, Memory: 17MB, Status: Passed - CodeTree

--- 250213/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 250213/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -2,21 +2,31 @@ n = int(input())
 price = list(map(int, input().split()))
 
 # Write your code here!
-min_val = 2**31-1
-for p in price:
-    if p < min_val:
-        min_val = p
+# min_val = 2**31-1
+# for p in price:
+#     if p < min_val:
+#         min_val = p
 
-cnt = price.index(min_val)
-max_val = 0
-for i in range(cnt+1, n):
-    if price[i] > max_val:
-        max_val = price[i]
+# cnt = price.index(min_val)
+# max_val = 0
+# for i in range(cnt+1, n):
+#     if price[i] > max_val:
+#         max_val = price[i]
 
-# print('min_val:', min_val)
-# print('max_val:', max_val)
+# # print('min_val:', min_val)
+# # print('max_val:', max_val)
 
-if max_val == 0:
-    print(0)
-else:
-    print(max_val - min_val)
+# if max_val == 0:
+#     print(0)
+# else:
+#     print(max_val - min_val)
+
+max_price = 0
+for i in range(n):
+    for j in range(n):
+        if price[i] < price[j] and i < j:
+            if price[j] - price[i] > max_price:
+                max_price = price[j] - price[i]
+
+print(max_price)
+
