d4487dcc4d888b997d94ebf28c7f28aedefa6774
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-19 16:14:42 +0900

    Title: 동전이 있는 위치, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250219/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,15 @@
+n, m = tuple(map(int, input().split()))
+
+arr_2d = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for _ in range(m):
+    x, y = tuple(map(int, input().split()))
+    arr_2d[x-1][y-1] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr_2d[i][j], end=' ')
+    print()
