69ad29c903da8ade0792a0c67cab60ce16753f2b
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-20 11:16:43 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250220/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    word = input()
+    arr.append(word)
+
+w = input()
+
+add_val = 0
+cnt = 0
+for a in arr:
+    if w in a:
+        add_val += len(a)
+        cnt += 1
+
+mean_val = add_val/cnt
+
+print(f"{cnt} {mean_val:.2f}")
+    
