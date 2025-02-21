5742da16813dbd7e3c646382083054e32df04316
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-21 14:19:05 +0900

    Title: Run Length 인코딩, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250221/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,18 @@
+sent = input()
+
+result = ''
+
+cnt = 0
+for i in range(len(sent)):
+    if i == 0 or sent[i] != sent[i-1]:
+        if i != 0:
+            result += f"{sent[i-1]}{str(cnt)}"
+        cnt = 1
+    elif sent[i] == sent[i-1]:
+        cnt += 1
+        if i == (len(sent)-1):
+            result += f"{sent[i-1]}{str(cnt)}"
+    
+
+print(len(result))
+print(result)
