dd492bdb8f77963b7a4113d766e3f914229b5302
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-26 16:34:14 +0900

    Title: 문자열 놀이, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/문자열 놀이/play-with-string.py
+++ 250226/문자열 놀이/play-with-string.py
@@ -1,19 +1,23 @@
 input_str, n = tuple(map(str, input().split()))
+
+before_word = input_str
 for _ in range(int(n)):
     cnt, a, b = tuple(map(str, input().split()))
     word = ''
     for i in range(len(input_str)):
         if cnt == '1':
             if i == (int(a)-1):
-                word += input_str[int(b)-1]
+                word += before_word[int(b)-1]
             elif i == (int(b)-1):
-                word += input_str[int(a)-1]
+                word += before_word[int(a)-1]
             else:
-                word += input_str[i]
-        if cnt == '2':
-            if input_str[i] == a:
+                word += before_word[i]
+        elif cnt == '2':
+            if before_word[i] == a:
                 word += b
             else:
-                word += input_str[i]
-
+                word += before_word[i]
+    
     print(word)
+
+    before_word = word
