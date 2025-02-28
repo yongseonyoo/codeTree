aaa6f6e47a9b9d9c60533f5187c0c853728f2521
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-02-28 11:28:21 +0900

    Title: 문자 제거하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/문자 제거하기/del-char.py
+++ 250228/문자 제거하기/del-char.py
@@ -2,7 +2,7 @@ s = input()
 n = len(s)
 arr = list(s)
 for i in range(20):
-    if len(arr) == 0:
+    if len(arr) == 1:
         break
     else:
         num = int(input())
@@ -15,4 +15,3 @@ for i in range(20):
             arr.pop(-1)
             f = ''.join(arr)
             print(f)
-            break
