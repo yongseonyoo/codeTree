48416852f78bec42f5542bafc1d038a427f3cd6c
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-03-05 16:14:34 +0900

    Title: 최소공배수 구하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250305/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250305/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,9 +2,9 @@ n, m = map(int, input().split())
 
 # Please write your code here.
 def find_lcm(n, m):
-    save_val = 100
-    min_val = 99
-    for i in range(100):
+    save_val = 1000
+    min_val = 999
+    for i in range(1000):
         if min_val > 0 and min_val % n == 0 and min_val % m == 0:
             if save_val > min_val:
                 save_val = min_val
