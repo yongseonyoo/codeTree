836c0a87b1f7287e99b8d3b6c0ace07dc364fa7b
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-03-06 10:38:21 +0900

    Title: 정수의 최솟값, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250306/정수의 최솟값/minimum-value-of-an-integer.py
@@ -0,0 +1,15 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+def min_val(a, b, c):
+    val = 0
+    if a < val:
+        val = a
+    if b < val:
+        val = b
+    if c < val:
+        val = c
+    
+    print(val)
+
+min_val(a, b, c)
