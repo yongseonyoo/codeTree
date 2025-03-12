3860ca8c380e507829ef934b82ec58ad76165a6a
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-03-12 18:00:46 +0900

    Title: 함수를 이용한 369 게임, Time: 189ms, Memory: 40MB, Status: Passed - CodeTree

--- 250312/함수를 이용한 369 게임/369-games-using-functions.py
+++ 250312/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,14 +3,17 @@ a, b = map(int, input().split())
 # Please write your code here.
 trg = ['3', '6', '9']
 
+get_list = []
+
 cnt = 0
 for i in range(a, b+1):
     n = list(str(i))
     if i % 3 == 0:
-        cnt += 1
+        get_list.append(i)
     else:
         for j in n:
             if j in trg:
-                cnt += 1
+                get_list.append(i)
 
-print(cnt)
+get_list = set(get_list)
+print(len(get_list))
