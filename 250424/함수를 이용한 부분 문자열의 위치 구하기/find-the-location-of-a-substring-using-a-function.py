4012ac9f19803f846ab9e0cd0049aa2309c504d1
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-04-24 17:08:06 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250424/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 250424/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -3,18 +3,29 @@ pattern = input()
 
 # Please write your code here.
 def hamsu(text, pattern):
-    if pattern[0] in text:
-        for i in range(len(text)-len(pattern)+1):
-            cnt = 1
-            if text[i] == pattern[0]:
-                # print(text[i], pattern[0])
-                for j in range(1, len(pattern)):
-                    # print(text[i+j], pattern[j])
-                    if text[i+j] == pattern[j]:
-                        cnt += 1
-                if cnt == len(pattern):
-                    print(i)
-                    break
+    flag = False
+    cnt = 0
+    for i in range(len(pattern)):
+        if pattern[i] in text:
+            cnt += 1
+    if cnt == len(pattern):
+        flag = True
+    
+    if flag == True:
+        if pattern[0] in text:
+            for i in range(len(text)-len(pattern)+1):
+                cnt = 1
+                if text[i] == pattern[0]:
+                    # print(text[i], pattern[0])
+                    for j in range(1, len(pattern)):
+                        # print(text[i+j], pattern[j])
+                        if text[i+j] == pattern[j]:
+                            cnt += 1
+                    if cnt == len(pattern):
+                        print(i)
+                        break
+        else:
+            print(-1)
     else:
         print(-1)
 
