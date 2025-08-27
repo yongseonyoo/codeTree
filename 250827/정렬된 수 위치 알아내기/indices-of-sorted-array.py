341f47e4c1d2d1b59d2013fd9704f77c91a3fff6
yongseonyoo <122426569+yongseonyoo@users.noreply.github.com>
2025-08-27 17:23:06 +0900

    Title: 정렬된 수 위치 알아내기, Time: 53ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250827/정렬된 수 위치 알아내기/indices-of-sorted-array.py
+++ 250827/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -6,8 +6,12 @@ index = sorted(sequence)
 
 idx = [(i+1, index[i]) for i in range(n)]
 
-for seq in sequence:
-    for i in idx:
-        if i[1] == seq:
-            print(i[0], end=' ')
-            idx.remove(i)
+if len(set(sequence)) == 1:
+    for i in range(n):
+        print(i+1, end=' ')
+else:
+    for seq in sequence:
+        for i in idx:
+            if i[1] == seq:
+                print(i[0], end=' ')
+                idx.remove(i)
